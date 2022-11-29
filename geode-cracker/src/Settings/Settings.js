eab79f0a062fb1937a4c50f11842ebeb6d10200e
Jasper Vonk <s1122268@student.hsleiden.nl>
2022-11-29 18:24:58 +0100

    feat: add settings component
    - rename instellingen route to settings
    - tweak index.css for inactive and active button functionalities
    - add MUI Core

---
+++ geode-cracker/src/Settings/Settings.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Settings.css';
+import Switch from '@mui/material/Switch';
+
+import IconRefresh from "./Images/settings_refresh.png";
+
+class Settings extends React.Component{
+    state = {
+        settings: "Instellingen",
+        dutch: "",
+        english: "--inactive",
+        vibration: "Vibratie",
+        sound: "Geluid",
+        reset: "Opnieuw proberen"
+    }
+
+    render(){
+        return(
+            <section class="settingsSection">
+                <h1 class="settingsSection__heading">{this.state.settings}</h1>
+                <article class="settingsSection__languages">
+                    <button class={"settingsSection__languages__button button button--green" + this.state.dutch}>Nederlands</button>
+                    <button class={"settingsSection__languages__button button button--green" + this.state.english}>English</button>
+                </article>
+                <article class="settingsSection__toggles">
+                    <section class="settingsSection__toggles__toggleOption">
+                        <h2 class="settingsSection__toggles__toggleOption__text">{this.state.vibration}</h2>
+                        <Switch defaultChecked />
+                    </section>
+                    <hr class="settingsSection__toggles__hr" />
+                    <section class="settingsSection__toggles__toggleOption">
+                        <h2 class="settingsSection__toggles__toggleOption__text">{this.state.sound}</h2>
+                        <Switch defaultChecked />
+                    </section>
+                    
+                </article>
+                <button class="settingsSection__button button button--red">
+                    {this.state.reset}
+                    <img class="settingsSection__button__image" src={IconRefresh}/>
+                </button>
+            </section>
+        )
+    }
+}
+
+export default Settings;
