4035e75bf9f68e8ac08af9696991e4812b98f09b
jaspervonk <s1122268@student.hsleiden.nl>
2022-12-04 15:04:45 +0100

    tweak(settingsconfirmation.css): change modal title to h2 tag

--- geode-cracker/src/Settings/Components/SettingsConfirmation.js
+++ geode-cracker/src/Settings/Components/SettingsConfirmation.js
@@ -12,7 +12,7 @@ class SettingsConfirmation extends React.Component{
         return(
             <section class="settingsSection__confirmationSection">
                 <article class="settingsSection__confirmationSection__modal">
-                    <h1 class="settingsSection__confirmationSection__modal__title">{this.state.title}</h1>
+                    <h2 class="settingsSection__confirmationSection__modal__title">{this.state.title}</h2>
                     <section class="settingsSection__confirmationSection__modal__choice">
                         <button class="settingsSection__confirmationSection__modal__choice__button button button--green">{this.state.text_button_left}</button>
                         <button class="settingsSection__confirmationSection__modal__choice__button button button--red">{this.state.text_button_right}</button>
