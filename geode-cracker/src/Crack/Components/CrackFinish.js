e262a77095a49d22196f441b3a01a3a4b94a5a42
Jasper Vonk <s1122268@student.hsleiden.nl>
2022-12-12 17:25:04 +0100

    feat: add crack page
    - add Crack.js and Crack.scss
    - add CrackPlace.js and CrackPlace.scss
    - add CrackLever.js and CrackLever.scss
    - add CrackFinish.js and CrackFinish.scss
    - add crack page images
    - add button--triggerbox styling to index.scss

---
+++ geode-cracker/src/Crack/Components/CrackFinish.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './CrackFinish.scss';
+
+
+class CrackFinish extends React.Component{
+    state = {
+        crack_action: "placeholder",
+        crack_finish_image_path: "/img/crack_finish.png"
+    };
+
+    render(){
+        return(
+            <section class="crackFinish">
+            <figure class="crackFinish__figure">
+                <button class="crackFinish__figure__button button--triggerbox" onClick={() => this.props.changeContent("crack_place")}>LEVER</button>
+                <img class="crackFinish__figure__image" src={this.state.crack_finish_image_path}></img>
+            </figure>
+        </section>
+        )
+    }
+}
+
+export default CrackFinish;
