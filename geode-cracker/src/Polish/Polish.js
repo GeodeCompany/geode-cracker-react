1cea8f7522e0a117d14f94c295203ac203349d27
MetSpek <daniel@realafford.nl>
2022-12-12 16:29:46 +0100

    refactor: rewrite brush interaction

--- geode-cracker/src/Polish/Polish.js
+++ geode-cracker/src/Polish/Polish.js
@@ -4,14 +4,15 @@ import './Polish.scss';
 class Polish extends React.Component{
 
     state = {
-        action_text: "Borstel de stukken los",
+        brush_text: "Borstel de stukken los",
+        wash_text: "Spoel de stukken weg",
+        polish_text: "Poets de geode schoon",
 
-        visibilityBucket: "hidden",
-        visibilityCloth: "hidden",
-        visibilityBrush: "show",
-        visibilityRock: "show",
-        visibilityShimmer1: "hidden",
-        visibilityShimmer2: "hidden",
+        start_positions: [[1.5, 22],[2.5, 4.5],[26, 24],[26, 0]],
+        end_positions: [[27, 15],[28, 7],[23, 20],[21, 11]],
+
+        interactionState: "brush",
+        rocksClicked: [false, false, false, false],
 
         geode_image_path: "/img/geode.png",
         bucket_normal_image_path: "/img/bucket_normal.png",
@@ -26,89 +27,84 @@ class Polish extends React.Component{
         glimmer2_image_path: "/img/glimmers2.png"
     }
 
-    interactionState = "brush";
-    rocksClicked = [false, false, false, false]
-
-    updateVisuals(state){
-      switch (state) {
+    updateVisuals(){
+      switch (this.state.interactionState) {
         case "wash":
-          this.state.visibilityBrush = "hidden";
-          this.state.visibilityBucket = "show";
+          document.getElementById("action_text").innerHTML = this.state.wash_text;
+          document.getElementById("brush_img").style.visibility = "hidden";
+          document.getElementById("bucket_img").style.visibility = "visible";
           break;
         case "polish":
-          this.state.visibilityBucket = "hidden";
-          this.state.visibilityCloth = "show";
+          document.getElementById("action_text").innerHTML = this.state.polish_text;
+          document.getElementById("bucket_img").style.visibility = "hidden";
+          for(var i = 0; i < document.getElementsByClassName("polish__section__figure__rock").length; i++) {
+            document.getElementsByClassName("polish__section__figure__rock")[i].style.visibility = "hidden";
+          }
+          document.getElementById("cloth_img").style.visibility = "visible";
           break;
       }
     }
 
-
-    rockClick(rock){
-      if(this.rocksClicked[rock - 1] == false){
-        this.rocksClicked[rock - 1] = true;
-        this.moveRock(rock);
+    rockClick(rock_number){
+      if(this.state.rocksClicked[rock_number - 1] == false){
+        this.state.rocksClicked[rock_number - 1] = true;
+        this.moveRock(rock_number);
         this.checkBrushDone();
       }
     }
 
-    moveRock(rock){
-      switch (rock) {
-        case 1:
-          document.getElementById("rock1").style.top = "26rem";
-          document.getElementById("rock1").style.right = "15rem";
-          break;
-        case 2:
-          document.getElementById("rock2").style.bottom = "4rem";
-          document.getElementById("rock2").style.right = "7rem";
-          break;
-        case 3:
-          document.getElementById("rock3").style.bottom = "1 rem";
-          document.getElementById("rock3").style.left = "6rem";
-          break;
-        case 4:
-          document.getElementById("rock4").style.top = "20rem";
-          document.getElementById("rock4").style.left = "15rem";
-          break;
-      }
+    moveRock(rock_number){
+      var rockString = "rock" + rock_number + "_img";
+      var positions = this.state.end_positions[rock_number - 1];
+      document.getElementById(rockString).style.top = positions[0] + "rem";
+      document.getElementById(rockString).style.left = positions[1] + "rem";
     }
 
     checkBrushDone(){
-      const getRocksClicked = this.rocksClicked.filter(Boolean).length;
+      const getRocksClicked = this.state.rocksClicked.filter(Boolean).length;
       if(getRocksClicked >= 4){
-        console.log("YOU DONE");
+        this.state.interactionState = "wash";
+        console.log(this.state.interactionState)
+        this.updateVisuals();
       }
     }
 
-    render(){
-        this.updateVisuals(this.interactionState);
+    bucketClick(){
+      console.log("CLICKED DA BUCKET");
 
+
+      this.state.interactionState = "polish"
+      this.updateVisuals();
+    }
+
+    render(){
         return(
         <article class="polish">
-            <button class="polish__button" style={{visibility: this.state.visibilityBucket}}>
+            <button class="polish__button" id="bucket_img" onClick={() => this.bucketClick()}>
                 <img class="polish__button__img" src={this.state.bucket_normal_image_path}></img>
             </button>
 
-            <h1 class="polish__text">{this.state.action_text}</h1>
+            <h1 class="polish__text" id="action_text">{this.state.brush_text}</h1>
 
             <section class="polish__section">
                 <figure class="polish__section__figure">
                     <img class="polish__section__figure__geode" src={this.state.geode_image_path}></img>
 
-                    <img class="polish__section__figure__rock polish__section__figure__rock--1" id="rock1" src={this.state.rock1_image_path} onClick={() => this.rockClick(1)} style={{visibility: this.state.visibilityRock}}></img>
-                    <img class="polish__section__figure__rock polish__section__figure__rock--2" id="rock2" src={this.state.rock2_image_path} onClick={() => this.rockClick(2)} style={{visibility: this.state.visibilityRock}}></img>
-                    <img class="polish__section__figure__rock polish__section__figure__rock--3" id="rock3" src={this.state.rock3_image_path} onClick={() => this.rockClick(3)} style={{visibility: this.state.visibilityRock}}></img>
-                    <img class="polish__section__figure__rock polish__section__figure__rock--4" id="rock4" src={this.state.rock4_image_path} onClick={() => this.rockClick(4)} style={{visibility: this.state.visibilityRock}}></img>
+                    <img class="polish__section__figure__rock polish__section__figure__rock--1" id="rock1_img" src={this.state.rock1_image_path} style={{top: this.state.start_positions[0][0] + 'rem', left: this.state.start_positions[0][1] + 'rem'}} onClick={() => this.rockClick(1)}></img>
+                    <img class="polish__section__figure__rock polish__section__figure__rock--2" id="rock2_img" src={this.state.rock2_image_path} style={{top: this.state.start_positions[1][0] + 'rem', left: this.state.start_positions[1][1] + 'rem'}} onClick={() => this.rockClick(2)}></img>
+                    <img class="polish__section__figure__rock polish__section__figure__rock--3" id="rock3_img" src={this.state.rock3_image_path} style={{top: this.state.start_positions[2][0] + 'rem', left: this.state.start_positions[2][1] + 'rem'}} onClick={() => this.rockClick(3)}></img>
+                    <img class="polish__section__figure__rock polish__section__figure__rock--4" id="rock4_img" src={this.state.rock4_image_path} style={{top: this.state.start_positions[3][0] + 'rem', left: this.state.start_positions[3][1] + 'rem'}} onClick={() => this.rockClick(4)}></img>
 
-                    <img class="polish__section__figure__glimmer polish__section__figure__glimmer--1" src={this.state.glimmer1_image_path} style={{visibility: this.state.visibilityShimmer1}}></img>
-                    <img class="polish__section__figure__glimmer polish__section__figure__glimmer--2" src={this.state.glimmer2_image_path} style={{visibility: this.state.visibilityShimmer2}}></img>
+                    <img class="polish__section__figure__glimmer polish__section__figure__glimmer--1" id="shimmer1" src={this.state.glimmer1_image_path}></img>
+                    <img class="polish__section__figure__glimmer polish__section__figure__glimmer--2" id="shimmer2" src={this.state.glimmer2_image_path}></img>
                 </figure>
             </section>
 
-            <div class="polish__moveable polish__moveable--brush" style={{visibility: this.state.visibilityBrush}}>
+            <div class="polish__moveable polish__moveable--brush" id="brush_img">
                 <img class="polish__moveable __img" src={this.state.brush_image_path}></img>
             </div>
 
-            <div class="polish__moveable polish__moveable--cloth" style={{visibility: this.state.visibilityCloth}}>
+            <div class="polish__moveable polish__moveable--cloth" id="cloth_img">
                 <img class="polish__moveable __img" src={this.state.cloth_image_path}></img>
             </div>
         </article>
