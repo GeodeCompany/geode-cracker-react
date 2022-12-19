2e61096be4be585052e5a1a111437b3797586c71
MetSpek <daniel@realafford.nl>
2022-12-19 17:27:25 +0100

    feat: add mascot help toggle

--- geode-cracker/src/Mascot/MascotHelp.js
+++ geode-cracker/src/Mascot/MascotHelp.js
@@ -4,19 +4,41 @@ import './MascotHelp.scss';
 class MascotHelp extends React.Component{
 
     state = {
-        mascot_inactive_image_path: "/img/mascot_inactive.png",
-        mascot_active_image_path: "/img/mascot_active.png",
+      mascot_active: false,
+
+      mascot_inactive_image_path: "/img/mascot_inactive.png",
+      mascot_active_image_path: "/img/mascot_active.png",
+    }
+
+    toggleContent(){
+      if(this.state.mascot_active == false){
+        this.changeVisibility("visible");
+        this.changeImage(this.state.mascot_active_image_path);
+        this.state.mascot_active = true;
+      } else {
+        this.changeVisibility("hidden");
+        this.changeImage(this.state.mascot_inactive_image_path);
+        this.state.mascot_active = false;
+      }
+    }
+
+    changeVisibility(state){
+      document.getElementById("help_text").style.visibility = state
+    }
+
+    changeImage(img){
+      document.getElementById("mascot_img").src = img
     }
 
     render(){
         return(
-        <article class="help">        
-            <figure class="help__figure">
-                <img class="help__figure__image" src={this.state.mascot_inactive_image_path}></img>
+        <article class="help">
+            <figure class="help__figure" onClick={() => this.toggleContent()}>
+                <img class="help__figure__image" id="mascot_img" src={this.state.mascot_inactive_image_path}></img>
             </figure>
-            <section class="help__dialogue">
-                <p class="help__dialogue__text">{this.props.mascot_text}</p>
+            <section class="help__dialogue" id="help_text">
                 <div class="help__dialogue__arrow"></div>
+                <p class="help__dialogue__text">{this.props.mascot_text}</p>
             </section>
         </article>
         )
