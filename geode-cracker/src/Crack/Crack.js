8329e20880cfd0db36428a714d155ca40bb10ad5
Jasper Vonk <s1122268@student.hsleiden.nl>
2022-12-20 19:08:29 +0100

    feat(crack.js): make modular and add animation
    - removed all crack/components
    - add animations
    - add modular options for different geodes

--- geode-cracker/src/Crack/Crack.js
+++ geode-cracker/src/Crack/Crack.js
@@ -1,70 +1,96 @@
 import React from 'react';
 import './Crack.scss';
 
-import CrackPlace from './Components/CrackPlace';
-import CrackLever from './Components/CrackLever';
-import CrackFinish from './Components/CrackFinish';
-
 class Crack extends React.Component{
     state = {
         startup_state: true,
         crack_action_text: "Plaats de Geode",
-        crack_place_state: false,
-        crack_lever_state: false,
-        crack_finish_state: false
+        crack_action_text_place: "placeholder",
+        crack_action_text_lever: "placeholder",
+        crack_action_text_finish: "placeholder",
+
+        crack_mascot_text_place: "placeholder",
+        crack_mascot_text_lever: "placeholder",
+        crack_mascot_text_finish: "placeholder",
+
+        crack_lever_state: "up",
+        crack_geode_dirty_left_state: "",
+        crack_geode_dirty_right_state: "",
+
+        crack_geode_base_image_path: "/img/geode_base.png",
+        crack_geode_cracked_image_path: "/img/geode_cracked.png",
+        crack_geode_dirty_image_path: "/img/geode_dirty.png",
+        crack_geode_outline_image_path: "/img/geode_outline.png",
+        crack_press_base_image_path: "/img/press_base.png",
+        crack_press_top_image_path: "/img/press_top.png",
+        crack_press_stamp_image_path: "/img/press_stamp.png",
+        crack_press_lever_image_path: "/img/press_lever.png",
     };
 
     changeContent(name){
-        this.resetContent();
         switch (name) {
-            case "crack_place":
-                this.props.updateMascotText("crack_place");
-                this.changeActionText("Plaats de Geode");
-                this.setState({crack_place_state: true});
+            case "crack_placed":
+                document.getElementById("geode_outline").style.display = "none";
+                document.getElementById("triggerbox_crack_placed").style.display = "none";
+
+                document.getElementById("geode_base").style.display = "block";
+                document.getElementById("triggerbox_crack_lever_up").style.display = "block";
                 break;
-            case "crack_lever":
-                this.props.updateMascotText("crack_lever");
-                this.changeActionText("Kraak de Geode");
-                this.setState({crack_lever_state: true});
+            case "crack_lever_up":
+                document.getElementById("triggerbox_crack_lever_up").style.display = "none";
+                this.setState({crack_lever_state:"down"});
+                setTimeout(() => document.getElementById("geode_base").style.display = "none", 1000);
+                setTimeout(() => document.getElementById("geode_cracked").style.display = "block", 1000);
+                setTimeout(() => document.getElementById("triggerbox_crack_lever_down").style.display = "block", 1000);
                 break;
-            case "crack_finish":
-                this.props.updateMascotText("crack_finish");
-                this.changeActionText("Gelukt!");
-                this.setState({crack_finish_state: true});
-                break;
-            default:
+            case "crack_lever_down":
+                document.getElementById("geode_cracked").style.display = "none";
+                document.getElementById("triggerbox_crack_lever_down").style.display = "none";
+                this.setState({crack_lever_state:"up"});
+  
+                document.getElementById("geode_dirty_left").style.display = "block";
+                document.getElementById("geode_dirty_right").style.display = "block";
+                setTimeout(() => document.getElementById("geode_dirty_left").classList.add("crack__figure__image--geode--dirty--left"), 100);
+                setTimeout(() => document.getElementById("geode_dirty_right").classList.add("crack__figure__image--geode--dirty--right"), 100);
                 break;
         }
+    }
+    
+    updateData(data_JSON, geode){
+        this.setState({
+            crack_action_text_place: "replaced",
+            crack_action_text_lever: "replaced",
+            crack_action_text_finish: "replaced",
+
+            crack_mascot_help_place: "replaced",
+            crack_mascot_help_lever: "replaced",
+            crack_mascot_help_finish: "replaced",
+        })
     }
 
     changeActionText(text){
         this.setState({crack_action_text: text});
     }
 
-    resetContent(){
-        this.setState({
-            crack_place_state: false,
-            crack_lever_state: false,
-            crack_finish_state: false
-        })
-    }
-
     render(){
-        if(this.state.startup_state){
-            this.changeContent("crack_place");
-            this.setState({
-                startup_state: false
-            })
-        }
+        // this.updateData(this.props.data_JSON, this.props.data_geode);
         return(
             <section class="crack">
                 <h1 class="crack__action">{this.state.crack_action_text}</h1>
-                {(this.state.crack_place_state && <CrackPlace changeContent={this.changeContent.bind(this)} changeActionText={this.changeActionText.bind(this)}/>)  
-                ||
-                (this.state.crack_lever_state && <CrackLever changeContent={this.changeContent.bind(this)} changeActionText={this.changeActionText.bind(this)}/>)
-                ||
-                (this.state.crack_finish_state && <CrackFinish changeContent={this.props.changeContent} crack_end_content={this.props.crack_end_content} changeActionText={this.changeActionText.bind(this)}/>)
-                }
+                <figure class="crack__figure">
+                    <img id="geode_base" class="crack__figure__image--geode" src={this.state.crack_geode_base_image_path}></img>
+                    <img id="geode_cracked" class="crack__figure__image--geode" src={this.state.crack_geode_cracked_image_path}></img>
+                    <img id="geode_dirty_left" class="crack__figure__image--geode" src={this.state.crack_geode_dirty_image_path}></img>
+                    <img id="geode_dirty_right" class="crack__figure__image--geode" src={this.state.crack_geode_dirty_image_path}></img>
+                    <img id="geode_outline" class="crack__figure__image--geode" src={this.state.crack_geode_outline_image_path}></img>
+                    <img id="press_base" class="crack__figure__image--press--base" src={this.state.crack_press_base_image_path}></img>
+                    <img id="press_lever" class={"crack__figure__image--press--lever crack__figure__image--press--lever--" + this.state.crack_lever_state} src={this.state.crack_press_lever_image_path}></img>
+                    <img id="press_stamp" class={"crack__figure__image--press--stamp crack__figure__image--press--stamp--" + this.state.crack_lever_state} src={this.state.crack_press_stamp_image_path}></img>
+                    <img id="press_top" class="crack__figure__image--press--top" src={this.state.crack_press_top_image_path}></img>
+                    <button id="triggerbox_crack_placed" class="crack__figure__button crack__figure__button--placed button--triggerbox" onClick={() => this.changeContent("crack_placed")}>LEVER</button>
+                    <button id="triggerbox_crack_lever_up" class="crack__figure__button crack__figure__button--lever--up button--triggerbox" onClick={() => this.changeContent("crack_lever_up")}>LEVER</button>
+                    <button id="triggerbox_crack_lever_down" class="crack__figure__button crack__figure__button--lever--down button--triggerbox" onClick={() => this.changeContent("crack_lever_down")}>LEVER</button>
+                </figure>
             </section>
         )
     }
