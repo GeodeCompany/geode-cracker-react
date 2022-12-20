aa48595a55a44f3e90e3478bf64a74451f2dccae
Jasper Vonk <s1122268@student.hsleiden.nl>
2022-12-20 23:18:53 +0100

    tempfix(settings.js): apply languages

--- geode-cracker/src/Settings/Components/SettingsMenu.js
+++ geode-cracker/src/Settings/Components/SettingsMenu.js
@@ -34,14 +34,28 @@ class SettingsMenu extends React.Component{
     }
 
     render(){
+        var title;
+        var vibrations;
+        var restart;
         var language_button_nl = <button class={"settingsMenu__languages__button button button--green"} onClick={() => this.updateLanguage("NL")} >Nederlands</button>;
         var language_button_en = <button class={"settingsMenu__languages__button button button--green"} onClick={() => this.updateLanguage("EN")} >English</button>;
-        if(this.props.data_language != "NL"){language_button_nl = <button class={"settingsMenu__languages__button button button--green--inactive"} onClick={() => this.updateLanguage("NL")} >Nederlands</button>;} 
-        if(this.props.data_language != "EN"){language_button_en = <button class={"settingsMenu__languages__button button button--green--inactive"} onClick={() => this.updateLanguage("EN")} >English</button>;}
+        if(this.props.data_language != "NL"){
+            language_button_nl = <button class={"settingsMenu__languages__button button button--green--inactive"} onClick={() => this.updateLanguage("NL")} >Nederlands</button>;
+            title = <h1 class="settingsMenu__heading">Settings</h1>;
+            vibrations = <h2 class="settingsMenu__toggles__toggleOption__text">Vibrations</h2>;
+            restart = <button class="settingsMenu__button button button--red" onClick={() => this.props.toggleComponent("confirmation_modal")}>Restart<img class="settingsMenu__button__image" src={this.state.icon_refresh_image_path}/></button>
+        } 
+        if(this.props.data_language != "EN"){
+            title = <h1 class="settingsMenu__heading">Instellingen</h1>;
+            vibrations = <h2 class="settingsMenu__toggles__toggleOption__text">Vibraties</h2>;
+            language_button_en = <button class={"settingsMenu__languages__button button button--green--inactive"} onClick={() => this.updateLanguage("EN")} >English</button>;
+            restart = <button class="settingsMenu__button button button--red" onClick={() => this.props.toggleComponent("confirmation_modal")}>Herstarten<img class="settingsMenu__button__image" src={this.state.icon_refresh_image_path}/></button>
+
+        }
         
         return(
             <section class="settingsMenu">
-                <h1 class="settingsMenu__heading">{this.state.title}</h1>
+                {title}
                 <article class="settingsMenu__languages">
                     {language_button_nl}
                     {language_button_en}
@@ -49,16 +63,12 @@ class SettingsMenu extends React.Component{
                 <article class="settingsMenu__toggles">
                     <hr class="settingsMenu__toggles__hr" />
                     <section class="settingsMenu__toggles__toggleOption">
-                        <h2 class="settingsMenu__toggles__toggleOption__text">{this.state.vibration}</h2>
+                        {vibrations}
                         <Switch checked={this.props.settings_vibrations} onChange={() => this.props.updateSettings("vibrations", !this.props.settings_vibrations)}/>
                     </section>
                     <hr class="settingsMenu__toggles__hr" />
-                    
                 </article>
-                <button class="settingsMenu__button button button--red" onClick={() => this.props.toggleComponent("confirmation_modal")}>
-                    {this.state.reset}
-                    <img class="settingsMenu__button__image" src={this.state.icon_refresh_image_path}/>
-                </button>
+                {restart}
             </section>
         )
     }
